b465c55bd096d19211550101d879f8f0d8de3971
Benhao Qu <s3773865@student.rmit.edu.au>
2021-05-18 22:53:55 +0800

    add Insert situation

--- src/bplustree/ListNode.java
+++ src/bplustree/ListNode.java
@@ -1,14 +1,28 @@
 package bplustree;
 
 public class ListNode{
-    public ListNode next;
+    public ListNode prev, next;
     // index key
     public String index;
     // implement a pointer to child treeNode
     public TNode child;
 
-    public ListNode(int degree){
+    public ListNode(){
+        this.prev = this.next = null;
+        this.index = null;
+        this.child = null;
+    }
+
+    public ListNode(String index){
+        this.prev = this.next = null;
+        this.index = index;
+        this.child = null;
+    }
+
+    public ListNode(ListNode last, String index){
+        this.prev = last;
         this.next = null;
+        this.index = index;
         this.child = null;
     }
 }
--- src/bplustree/TNode.java
+++ src/bplustree/TNode.java
@@ -1,14 +1,26 @@
 package bplustree;
 
 public abstract class TNode {
-    public ListNode root;
+    // the first and last ListNode
+    public ListNode root, last;
+    public TNode leftmost_child;
+    // maximum capacity: degree
     private final int degree;
     private int capacity;
     public TNode(int degree){
+        this.root = this.last = null;
+        this.leftmost_child = null;
         this.degree = degree;
         this.capacity = 0;
     }
 
+    /*
+    * Insert has three situation:
+    * 1. Both LeafNode and TreeNode are not full, insert into LeafNode directly
+    * 2. Only LeafNode is full, separate LeafNode into two, insert the middle index into TreeNode
+    * 3. Both LeafNode and TreeNode are full: separate LeafNode into two, separate TreeNode into Two,
+    *  insert the middle index of LeafNode into TreeNode, insert middle index of TreeNode into parent TreeNode
+    */
     public abstract void insert(String index);
 
     public abstract void delete(String index);
